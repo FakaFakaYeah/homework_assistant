7ffd7334896a8d501b4cd16dfc4f26ace1806812
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2022-10-10 17:56:38 +0300

    Бот-ассистент

--- homework.py
+++ homework.py
@@ -130,8 +130,7 @@ def main():
                     last_error = error
             except TelegramMessageError as error:
                 logger.error(error)
-        finally:
-            time.sleep(RETRY_TIME)
+        time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
